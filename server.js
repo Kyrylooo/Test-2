66a3568617abce27b8223c01afd1626e62eaceb1
Kyrylooo <146863438+Kyrylooo@users.noreply.github.com>
2024-03-02 22:16:46 +0200

    ок

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const PORT = 3000;
-
-const app = express();
-
-app.listen(PORT, (err) => {
-    err ? console.log(err) : console.log('Listening port ${PORT}');
-})
